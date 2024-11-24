ac7f9bd3ea820e30f6b219c5de7fdf8fd8ca3f1e
Jimmy Lundgren <qwesto@gmail.com>
2024-11-24 09:53:53 +0100

    Dragscroll toggle on click and momentary on hold.

--- keyboards/ploopyco/madromys/rev1_001/keymaps/hashat/keymap.c
+++ keyboards/ploopyco/madromys/rev1_001/keymaps/hashat/keymap.c
@@ -16,9 +16,42 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "config.h"
+
+extern bool is_drag_scroll;
+uint16_t overall_scroll_distance = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( KC_BTN4, KC_BTN5, DRAG_SCROLL, KC_BTN2, KC_BTN1, MO(1) ),
 	[1] = LAYOUT( KC_MPLY, KC_MNXT, QK_BOOT, DPI_CONFIG, KC_BTN3, XXXXXXX )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  /* Specially handle l/r clicks on drag-scrolling (stop scrolling & reset DPI)
+   */
+  if (is_drag_scroll && (keycode == KC_BTN1 || keycode == KC_BTN2)) {
+    is_drag_scroll = false;
+    pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
+    return false;
+  }
+
+  if (is_drag_scroll && keycode == DRAG_SCROLL && !record->event.pressed &&
+      overall_scroll_distance > PLOOPY_DRAGSCROLL_RELEASE_DISTANCE) {
+    /* If we scrolled further than that distance while holding the
+       button pressed, it's an adhoc scroll & we clear the
+       drag-scroll state. */
+    is_drag_scroll = false;
+    pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
+    return false;
+  }
+
+  return true;
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+  if (is_drag_scroll) {
+    overall_scroll_distance += abs(mouse_report.v) + abs(mouse_report.h);
+  } else {
+    overall_scroll_distance = 0;
+  }
+  return mouse_report;
+}
