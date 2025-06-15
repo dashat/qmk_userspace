86661e988d5605eefdcae4904b3187eca13c2747
Jimmy Lundgren <qwesto@gmail.com>
2025-06-15 08:27:47 +0200

    Switch from tap-toggle to layer-lock functionality. Fix misplaced SE_SCLN and SE_QUOT on base layer.

--- keyboards/dztech/tofu60/keymaps/hashat/keymap.c
+++ keyboards/dztech/tofu60/keymaps/hashat/keymap.c
@@ -123,41 +123,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BH] = LAYOUT_60_iso_split_bs_rshift(
         KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_MINS, SE_EQL,  SE_6,    SE_7,    SE_8,    SE_9,    SE_0, KC_DEL, SE_RBRC, 
         KC_TAB,  SE_Q,    SE_W,    SE_F,    SE_P,    SE_G,    SE_LBRC, SE_SLSH, SE_J,    SE_L,    SE_U,    SE_Y,    SE_ARNG,  
-        KC_BSPC, HOME_A,  HOME_R,  HOME_S,  HOME_T,  SE_ODIA, SE_QUOT, SE_SCLN, SE_ADIA, HOME_N,  HOME_E,  HOME_I,  HOME_O,  KC_ENT,
-        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_COMM, SE_DOT,  SE_B,    SE_H,    SE_M,    SE_K,    KC_RSFT, TT(_FH),
-        KC_LGUI, KC_LALT,    KC_LCTL,                            KC_SPC,                          KC_RALT,  TT(_FL), KC_RGUI, KC_RCTL
+        KC_BSPC, HOME_A,  HOME_R,  HOME_S,  HOME_T,  SE_ODIA, SE_SCLN, SE_QUOT, SE_ADIA, HOME_N,  HOME_E,  HOME_I,  HOME_O,  KC_ENT,
+        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_COMM, SE_DOT,  SE_B,    SE_H,    SE_M,    SE_K,    KC_RSFT, MO(_FH),
+        KC_LGUI, KC_LALT,    KC_LCTL,                            KC_SPC,                          KC_RALT,  MO(_FL), KC_RGUI, KC_RCTL
     ), 
 	//base without homerow mods
     [_BL] = LAYOUT_60_iso_split_bs_rshift(
         KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_MINS, SE_EQL,  SE_6,    SE_7,    SE_8,    SE_9,    SE_0, KC_DEL, SE_RBRC, 
         KC_TAB,  SE_Q,    SE_W,    SE_F,    SE_P,    SE_G,    SE_LBRC, SE_SLSH, SE_J,    SE_L,    SE_U,    SE_Y,    SE_ARNG,  
         KC_BSPC, SE_A,    SE_R,    SE_S,    SE_T,    SE_ODIA, SE_SCLN, SE_QUOT, SE_ADIA, SE_N,    SE_E,    SE_I,    SE_O,  KC_ENT,
-        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_COMM, SE_DOT,  SE_B,    SE_H,    SE_M,    SE_K,    KC_RSFT, TT(_FH),
-        KC_LGUI, KC_LALT,    KC_LCTL,                            KC_SPC,                          KC_RALT,  TT(_FL),  KC_RGUI, KC_RCTL
+        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_COMM, SE_DOT,  SE_B,    SE_H,    SE_M,    SE_K,    KC_RSFT, MO(_FH),
+        KC_LGUI, KC_LALT,    KC_LCTL,                            KC_SPC,                          KC_RALT,  MO(_FL),  KC_RGUI, KC_RCTL
     ),
 	//normal qwerty
 	[_QW] = LAYOUT_60_iso_split_bs_rshift(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_GRV, 
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TT(_FH),
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, TT(_FL), TG(_QW), KC_RCTL
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FH),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FL), TG(_QW), KC_RCTL
     ),
 	//numpad and navigation with HRM
 	[_FH] = LAYOUT_60_iso_split_bs_rshift(
         KC_NUM , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   ALT_F4,  QK_BOOT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F12,  KC_F11,
 		_______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______,
         _______, _______, HOME_P4, HOME_P5, HOME_P6, KC_PDOT, KC_MPRV, KC_MNXT, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_ENT,
-        _______, KC_PMNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, TO(_BH),
-        _______, _______, _______,                              KC_PENT,                         _______,  TG(_BL), TG(_QW), _______
+        _______, KC_PMNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, QK_LLCK,
+        _______, _______, _______,                              KC_PENT,                         _______,  QK_LLCK, TG(_QW), _______
     ),
 	//numpad and navigation no HRM
 	[_FL] = LAYOUT_60_iso_split_bs_rshift(
         KC_NUM , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   ALT_F4,  QK_BOOT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F12,  KC_F11,
 		_______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______,
         _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_MPRV, KC_MNXT, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_ENT,
-        _______, KC_PMNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, TO(_BH),
-        _______, _______, _______,                              KC_PENT,                         _______,  TG(_BL), TG(_QW), _______
+        _______, KC_PMNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, QK_LLCK,
+        _______, _______, _______,                              KC_PENT,                         _______,  QK_LLCK, TG(_QW), _______
     )
 
 };
