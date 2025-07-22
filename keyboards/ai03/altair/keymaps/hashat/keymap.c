41f60a06bb82751b6ca6bb355786271f17711117
Jimmy Lundgren <qwesto@gmail.com>
2025-07-22 14:29:28 +0200

    Add KC_F14.

--- keyboards/ai03/altair/keymaps/hashat/keymap.c
+++ keyboards/ai03/altair/keymaps/hashat/keymap.c
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FH] = LAYOUT(
         KC_NUM,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   ALT_F4,  QK_BOOT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_VOLU, KC_MPRV, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_F12,
-        _______, _______, HOME_P4, HOME_P5, HOME_P6, KC_PDOT, KC_VOLD, KC_MNXT, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+        _______, _______, HOME_P4, HOME_P5, HOME_P6, KC_PDOT, KC_VOLD, KC_MNXT, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_F14,
         _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MUTE, KC_MPLY, _______, _______, _______, _______, _______, _______,
                                    TG(_FL), KC_P0,   KC_PENT, TG(_GL), _______, KC_TAB,  TG(_BL), TG(_FL)
     ), 	
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GF] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
         _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, 
-        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, 
+        KC_F14,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_F14, 
         KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ENT, 
                                    TG(_GL), _______, _______, _______, _______, _______, _______, TG(_GL) 
     ),
