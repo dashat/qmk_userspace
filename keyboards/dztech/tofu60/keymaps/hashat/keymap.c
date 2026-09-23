40b9f794ce787f02dfbb842e1eec039eaaaee5ae
Jimmy Lundgren <qwesto@gmail.com>
2026-09-23 12:25:58 +0200

    Small housekeeping

--- keyboards/dztech/tofu60/keymaps/hashat/keymap.c
+++ keyboards/dztech/tofu60/keymaps/hashat/keymap.c
@@ -182,7 +182,7 @@ bool caps_word_press_user(uint16_t keycode) {
         default:
             return false;  // Deactivate Caps Word.
     }
-}
+};
 
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
