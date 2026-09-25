ac080dddcbaabf385b0d52dc68d9fc2cf988d2b5
Jimmy Lundgren <qwesto@gmail.com>
2026-09-25 17:46:42 +0200

    Change gaming layer to Cölemak. Update QWERTY-layer with åäö.

--- keyboards/ai03/altair/keymaps/hashat/keymap.c
+++ keyboards/ai03/altair/keymaps/hashat/keymap.c
@@ -134,12 +134,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	//normal qwerty - needs fixing for SE maybe?
 	[_QW] = LAYOUT(
+        KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_MINS, SE_EQL,  SE_6,    SE_7,    SE_8,    SE_9,    SE_0,    KC_DEL,
+        KC_TAB,  SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    SE_LBRC, SE_RBRC, SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,    SE_ARNG,
+        KC_BSPC, SE_A,    SE_S,    SE_D,    SE_F,    SE_G,    SE_LPRN, SE_RPRN, SE_H,    SE_J,    SE_K,    SE_L,    SE_ODIA, SE_ADIA,
+        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,    SE_QUOT, KC_NUBS, SE_N,    SE_M,    SE_COMM, SE_DOT,  SE_SLSH, KC_ENT,
+                                   KC_LALT, KC_LCTL, KC_SPC,  MO(_FH), MO(_FH), KC_SPC,  KC_RGUI, KC_RALT
+    ),
+	/* 
+	[_QW] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NUHS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ARNG,
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_NUBS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                    KC_LALT, KC_LCTL, KC_SPC,  MO(_FH), MO(_FH), KC_SPC,  KC_RGUI, KC_RALT
-    ),
+    ), 
+	*/
 	//numpad+nav+media+f-row with HRM
     [_FH] = LAYOUT(
         KC_NUM,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   ALT_F4,  QK_BOOT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
@@ -153,10 +162,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   ALT_F4,  QK_BOOT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_VOLU, KC_MPRV, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_F12,
         _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_VOLD, KC_MNXT, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-        _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MUTE, KC_MPLY, _______, _______, _______, _______, _______, _______,
+        TO(_BH), KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_PSLS, KC_MUTE, KC_MPLY, _______, _______, _______, _______, _______, TO(_BH),
                                    TO(_BH), KC_P0,   KC_PENT, _______, _______, KC_TAB,  _______, TO(_BH)
     ),	
 	//gaming
+    [_GL] = LAYOUT(
+        KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_MINS, SE_EQL,  SE_6,    SE_7,    SE_8,    SE_9,    SE_0,    KC_DEL,
+        KC_TAB,  SE_Q,    SE_W,    SE_F,    SE_P,    SE_G,    SE_LBRC, SE_RBRC, SE_J,    SE_L,    SE_U,    SE_Y,    SE_ARNG, SE_SCLN,
+        KC_BSPC, SE_A,    SE_R,    SE_S,    SE_T,    SE_ODIA, SE_LPRN, SE_RPRN, SE_ADIA, SE_N,    SE_E,    SE_I,    SE_O,    SE_QUOT,
+        KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_D,    SE_V,    SE_COMM, SE_DOT,  SE_B,    SE_H,    SE_M,    SE_K,    SE_SLSH, KC_ENT,
+                                   KC_LALT, KC_LCTL, KC_SPC,  MO(_GF), MO(_GF), KC_SPC,  KC_RGUI, KC_RALT
+    ),
+	/* 	
 	[_GL] = LAYOUT(
         KC_ESC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_MINS, SE_EQL,  SE_6,    SE_7,    SE_8,    SE_9,    SE_0,    KC_DEL,
         KC_TAB,  SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    SE_LBRC, SE_RBRC, SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,    SE_ARNG,
@@ -164,6 +181,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,    KC_GRV,  KC_NUBS, SE_N,    SE_M,    SE_COMM, SE_DOT,  SE_SLSH, KC_ENT,
                                    KC_LALT, KC_LCTL, KC_SPC,  MO(_GF), MO(_GF), KC_SPC,  KC_RGUI, KC_RALT
     ),
+	*/
     /* 
 	[_GL] = LAYOUT(
         KC_ESC,  SE_5,    SE_4,    SE_3,    SE_2,    SE_1,    SE_EQL,  SE_EQL,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    KC_ESC, 
